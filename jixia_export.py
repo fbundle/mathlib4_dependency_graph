2113957ec5981d106954ed3f3c27d3ad062c47a4
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-19 17:28:53 +0800

    auto_commit

--- jixia_export.py
+++ jixia_export.py
@@ -1,14 +1,23 @@
 import os
 
 import jixia_util as jixia
+from jixia_util.structs import LeanName
 
-jixia.run.executable = os.path.abspath("jixia/.lake/build/bin/jixia")
+JIXIA = "jixia/.lake/build/bin/jixia"
+MATHLIB_DIR = "mathlib4"
+MATHLIB_MODULE = "Mathlib"
+
+def to_jixia_module(module: str) -> LeanName:
+    return module.split(".")
+
+
+jixia.run.executable = os.path.abspath(JIXIA)
 
 project = jixia.LeanProject(
-    root=os.path.abspath("mathlib4"),
+    root=os.path.abspath(MATHLIB_DIR),
     output_dir=".jixia",
 )
 
 project.batch_run_jixia(
-    prefixes=["Mathlib"]
+    prefixes=[to_jixia_module(MATHLIB_MODULE)], 
 )
