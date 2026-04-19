64cae024a0487772dadfbd40876fb13e2dca94f7
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-19 17:11:04 +0800

    auto_commit

---
+++ jixia_export.py
@@ -0,0 +1,12 @@
+import os
+
+import jixia_util as jixia
+
+jixia.run.executable = os.path.abspath("jixia/.lake/build/bin/jixia")
+
+project = jixia.LeanProject(
+    root=os.path.abspath("mathlib4"),
+    output_dir=".jixia",
+)
+
+project.batch_run_jixia()
