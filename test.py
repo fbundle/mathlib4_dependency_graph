7b6a51a329412fcca521f9907e1393c1bcb2756a
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-20 15:52:10 +0800

    auto_commit

--- test.py
+++ test.py
@@ -45,36 +45,26 @@ def stream_json_list(filename: str) -> Iterator:
         for item in items:
             yield item
 
-def write_cache(env: lmdb.Environment, filename: str) -> bool:
-    file_key = get_file_key(filename)
-    with env.begin() as txn:
-        if txn.get(file_key.encode()) is not None:
-            return False
-    
-    item_list = []
-    path = os.path.join(graph_dir, filename)
-    for o in stream_json_list(path):
-        try:
-            i = JixiaItem.model_validate(o)
-            item_list.append(i)
-            if len(item_list) > 1000:
-                with env.begin(write=True) as txn:
-                    for i in item_list:
-                        txn.put(get_item_key(i).encode(), i.model_dump_json().encode())
-                item_list = []
-        except pydantic_core._pydantic_core.ValidationError:
-            pass
-    
-    with env.begin(write=True) as txn:
-        txn.put(file_key.encode(), b"visited")
-    return True
 
 filename_list = list(os.listdir(graph_dir))
 
+total_count, error_count = 0, 0
+
 with lmdb.open("cache", map_size=100 * 1024**3) as env: # max 100GB
-    with mp.Pool(4) as pool:  # type: ignore
-        for r in tqdm(
-            pool.imap_unordered(lambda filename: write_cache(env, filename), filename_list),
-            total=len(filename_list),
-        ):
-            pass
+    with env.begin(write=True) as txn:
+        for filename in filename_list:
+            file_key = get_file_key(filename)
+            if txn.get(file_key.encode()) is not None:
+                continue
+            
+            path = os.path.join(graph_dir, filename)
+            for o in stream_json_list(path):
+                total_count += 1
+                try:
+                    i = JixiaItem.model_validate(o)    
+                    txn.put(get_item_key(i).encode(), i.model_dump_json().encode())
+                        
+                except pydantic_core._pydantic_core.ValidationError:
+                    error_count += 1
+
+print(error_count, total_count)
