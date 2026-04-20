2ea350c095f48e9e8bd474f1b7ddf20b61732781
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-20 15:49:04 +0800

    auto_commit

--- test.py
+++ test.py
@@ -46,27 +46,33 @@ def stream_json_list(filename: str) -> Iterator:
             yield item
 
 def write_cache(env: lmdb.Environment, filename: str) -> bool:
-    with env.begin(write=True) as txn:
-        file_key = get_file_key(filename)
+    file_key = get_file_key(filename)
+    with env.begin() as txn:
         if txn.get(file_key.encode()) is not None:
             return False
-        
-        path = os.path.join(graph_dir, filename)
-        for o in stream_json_list(path):
-            try:
-                i = JixiaItem.model_validate(o)
-                item_key = get_item_key(i)
-                txn.put(item_key.encode(), i.model_dump_json().encode())
-            except pydantic_core._pydantic_core.ValidationError:
-                pass
-        
+    
+    item_list = []
+    path = os.path.join(graph_dir, filename)
+    for o in stream_json_list(path):
+        try:
+            i = JixiaItem.model_validate(o)
+            item_list.append(i)
+            if len(item_list) > 1000:
+                with env.begin(write=True) as txn:
+                    for i in item_list:
+                        txn.put(get_item_key(i).encode(), i.model_dump_json().encode())
+                item_list = []
+        except pydantic_core._pydantic_core.ValidationError:
+            pass
+    
+    with env.begin(write=True) as txn:
         txn.put(file_key.encode(), b"visited")
-        return True
+    return True
 
 filename_list = list(os.listdir(graph_dir))
 
 with lmdb.open("cache", map_size=100 * 1024**3) as env: # max 100GB
-    with mp.Pool() as pool:  # type: ignore
+    with mp.Pool(4) as pool:  # type: ignore
         for r in tqdm(
             pool.imap_unordered(lambda filename: write_cache(env, filename), filename_list),
             total=len(filename_list),
