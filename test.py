8112607c69105883f7670591bdaf2e866f95f912
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-20 15:55:41 +0800

    auto_commit

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import json
 import os
 from typing import Iterator
 
@@ -50,22 +51,24 @@ filename_list = list(os.listdir(graph_dir))
 
 total_count, error_count = 0, 0
 
-with lmdb.open("cache", map_size=100 * 1024**3) as env: # max 100GB
-    with env.begin(write=True) as txn:
-        for filename in filename_list:
-            file_key = get_file_key(filename)
-            if txn.get(file_key.encode()) is not None:
-                continue
-            
-            path = os.path.join(graph_dir, filename)
-            for o in stream_json_list(path):
-                total_count += 1
-                try:
-                    i = JixiaItem.model_validate(o)    
-                    txn.put(get_item_key(i).encode(), i.model_dump_json().encode())
-                        
-                except pydantic_core._pydantic_core.ValidationError:
-                    error_count += 1
-                    print("error ratio:", error_count/total_count)
+with open("error.jsonl", "w") as f:
+    with lmdb.open("cache", map_size=100 * 1024**3) as env: # max 100GB
+        with env.begin(write=True) as txn:
+            for filename in tqdm(filename_list):
+                file_key = get_file_key(filename)
+                if txn.get(file_key.encode()) is not None:
+                    continue
+                
+                path = os.path.join(graph_dir, filename)
+                for o in stream_json_list(path):
+                    total_count += 1
+                    try:
+                        i = JixiaItem.model_validate(o)    
+                        txn.put(get_item_key(i).encode(), i.model_dump_json().encode())
+                            
+                    except pydantic_core._pydantic_core.ValidationError:
+                        error_count += 1
+                        f.write(json.dumps(o) + "\n")
+                    
 
 print(error_count, total_count)
