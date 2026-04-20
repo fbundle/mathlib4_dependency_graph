f0a3bc2d7ea50c7039c05ac71a1a9f77eaf6be7c
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-20 15:45:20 +0800

    auto_commit

--- test.py
+++ test.py
@@ -13,6 +13,7 @@ import ijson
 from pydantic import BaseModel
 from tqdm import tqdm
 
+import multiprocess as mp
 
 type JixiaLeanName = list[str]
 
@@ -31,15 +32,11 @@ class JixiaItem(BaseModel):
 
 graph_dir = "output/mathlib4_dependency_graph/data_5e932f97dd25535344f80f9dd8da3aab83df0fe6"
 
-total_count = 0
-error_count = 0
-
 def get_item_key(item: JixiaItem) -> str:
     return "item." + ".".join(item.name)
 
-def get_file_key(name: str) -> str:
-    return "file." + name
-
+def get_file_key(filename: str) -> str:
+    return "file." + filename
 
 
 def stream_json_list(filename: str) -> Iterator:
@@ -48,25 +45,30 @@ def stream_json_list(filename: str) -> Iterator:
         for item in items:
             yield item
 
+def write_cache(env: lmdb.Environment, filename: str) -> bool:
+    with env.begin(write=True) as txn:
+        file_key = get_file_key(filename)
+        if txn.get(file_key.encode()) is not None:
+            return False
+        
+        path = os.path.join(graph_dir, filename)
+        for o in stream_json_list(path):
+            try:
+                i = JixiaItem.model_validate(o)
+                item_key = get_item_key(i)
+                txn.put(item_key.encode(), i.model_dump_json().encode())
+            except pydantic_core._pydantic_core.ValidationError:
+                pass
+        
+        txn.put(file_key.encode(), b"visited")
+        return True
+
+filename_list = list(os.listdir(graph_dir))
 
 with lmdb.open("cache", map_size=100 * 1024**3) as env: # max 100GB
-    with env.begin(write=True) as txn:
-        for name in tqdm(list(os.listdir(graph_dir))):
-            file_key = get_file_key(name)
-            if txn.get(file_key.encode()) is not None:
-                continue
-
-            path = os.path.join(graph_dir, name)
-            for o in stream_json_list(path):
-                total_count += 1
-                try:
-                    i = JixiaItem.model_validate(o)
-                    item_key = get_item_key(i)
-                    txn.put(item_key.encode(), i.model_dump_json().encode())
-                except pydantic_core._pydantic_core.ValidationError:
-                    error_count += 1
-            
-            txn.put(file_key.encode(), b"visited")
-    
-
-print(error_count, total_count)
+    with mp.Pool() as pool:  # type: ignore
+        for r in tqdm(
+            pool.imap_unordered(lambda filename: write_cache(env, filename), filename_list),
+            total=len(filename_list),
+        ):
+            pass
