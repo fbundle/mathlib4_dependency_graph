02bd6ab77f4a35319162942882dfa6fee7bad66d
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-20 15:21:17 +0800

    auto_commit

--- test.py
+++ test.py
@@ -38,6 +38,8 @@ graph_dir = "output/mathlib4_dependency_graph/data_5e932f97dd25535344f80f9dd8da3
 
 name_list = list(os.listdir(graph_dir))
 
+total_count = 0
+error_count = 0
 
 with lmdb.open("cache", map_size=100 * 1024**3) as env: # max 100GB
     with env.begin(write=True) as txn:
@@ -45,10 +47,12 @@ with lmdb.open("cache", map_size=100 * 1024**3) as env: # max 100GB
             path = os.path.join(graph_dir, name)
             o_list = json.load(open(path))
             for o in o_list:
+                total_count += 1
                 try:
                     i = JixiaItem.model_validate(o)
                     key = get_dot_name(i.name)
                     txn.put(key.encode(), i.model_dump_json().encode())
                 except pydantic_core._pydantic_core.ValidationError:
-                    print(o)
-                    sys.exit(1)
+                    error_count += 1
+
+print(error_count, total_count)
