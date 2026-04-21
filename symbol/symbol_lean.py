252b2f92e3c8e76acd0cace00b7ee86497128459
Nguyen Ngoc Khanh <nguyenng@a2ap-login02.asp2p.nscc.sg>
2026-04-21 13:35:58 +0800

    auto_commit

---
+++ symbol/symbol_lean.py
@@ -0,0 +1,4 @@
+from .symbol import Symbol
+
+def get_lean_template(symbol: Symbol) -> str
+    pass
--- test.py
+++ test.py
@@ -4,9 +4,18 @@ from symbol import SymbolDB
 
 def main():
     dbpath = "output/mathlib4_dependency_graph/symbol_5e932f97dd25535344f80f9dd8da3aab83df0fe6.db"
-    # verify our db
-    missing_name_set = set()
     with SymbolDB(dbpath) as kv:
+        # print all kinds
+        kinds = set()
+        for symbol in tqdm(kv, total=len(kv), desc="getting all kinds ..."):
+            kinds.add(symbol.kind)
+        
+        print(kinds)
+
+
+        return
+        # verify our db
+        missing_name_set = set()
         for symbol in tqdm(kv, total=len(kv), desc="verifying db ..."):
             references = set(symbol.typeReferences + symbol.valueReferences)
             for name in references:
@@ -15,7 +24,7 @@ def main():
                         missing_name_set.add(name)
                         print(len(missing_name_set), name)
     
-    print(missing_name_set)
+        print(missing_name_set)
 
 
 
