6a84e0401bce78a0f607e5b49d2379f33127c843
Nguyen Ngoc Khanh <nguyenng@a2ap-login02.asp2p.nscc.sg>
2026-04-21 13:07:50 +0800

    auto_commit

--- symbol_db.py
+++ symbol_db.py
@@ -23,34 +23,35 @@ class SymbolDB:
     def __init__(self, symbol_dbpath: str):
         self.symbol_dbpath = symbol_dbpath
         self.env = None
+        self.txn = None
     
     def __enter__(self) -> SymbolDB:
         self.env = lmdb.open(self.symbol_dbpath, readonly=True)
+        self.txn = self.env.begin().__enter__()
         return self
     
     def __exit__(self, exc_type, exc, tb):
+        self.txn.__exit__(exc_type, exc, tb)
         self.env.close()
         self.env = None
 
     def __getitem__(self, name: str) -> Symbol:
-        with self.env.begin() as txn:
-            symbol: bytes = txn.get(name.encode(ENCODING))
+        symbol: bytes = self.txn.get(name.encode(ENCODING))
         return Symbol.model_validate_json(symbol)
     
     def __len__(self) -> int:
         return self.env.stat()["entries"]
 
     def __iter__(self, prefix: str = "") -> Iterator[Symbol]:
-        with self.env.begin() as txn:
-            cursor = txn.cursor()
-            cursor.set_range(prefix.encode(ENCODING))
-            for key, val in cursor:
-                key_str = key.decode(ENCODING)
-                if not key_str.startswith(prefix):
-                    break
-                val_str = val.decode(ENCODING)
-                symbol = Symbol.model_validate_json(val_str)
-                yield symbol
+        cursor = self.txn.cursor()
+        cursor.set_range(prefix.encode(ENCODING))
+        for key, val in cursor:
+            key_str = key.decode(ENCODING)
+            if not key_str.startswith(prefix):
+                break
+            val_str = val.decode(ENCODING)
+            symbol = Symbol.model_validate_json(val_str)
+            yield symbol
 
 def main():
     dbpath = "output/mathlib4_dependency_graph/symbol_5e932f97dd25535344f80f9dd8da3aab83df0fe6.db"
